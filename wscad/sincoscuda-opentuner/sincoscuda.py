2d2ef2da414355693064c51c2014cdd6435e7d90
João Martins Filho <joaomfilho1995@gmail.com>
2017-08-05 23:45:05 -0300

    add sincos 320 occ sum 131072 gld

--- wscad/sincoscuda-opentuner/sincoscuda.py
+++ wscad/sincoscuda-opentuner/sincoscuda.py
@@ -35,9 +35,9 @@ from opentuner import Result
 # (name, min, max)
 BLOCO_PARAMETROS = [
   ('kernel', 0, 3),
-  ('nx', 224, 224),
-  ('ny', 224, 224),
-  ('nz', 224, 224),
+  ('nx', 320, 320),
+  ('ny', 320, 320),
+  ('nz', 320, 320),
   ('gpuId', 0, 0)  
 ]
 # Test para 2 1 1 64 1 1 1 64 64 64 0 0 
@@ -47,7 +47,7 @@ BLOCO_PARAMETROS_CONFIGS = [ 'config' ]
 
 def read_file_configs():
   #file_sincos_projetocuda = open('/home/projetocuda/Documentos/tcc_cuda_opentuner_joao/wscad/gen-configs/saida_sincos-96-96.txt','r')
-  file_sincos_titanx = open('/home/joao/tcc_cuda_opentuner_joao/wscad/gen-configs/saida_sincos-224-224.txt','r')
+  file_sincos_titanx = open('/home/joao/tcc_cuda_opentuner_joao/wscad/gen-configs/saida_sincos-320-320.txt','r')
   list_configs = []
   for linha in file_sincos_titanx:
     list_configs.append(linha)
@@ -196,7 +196,7 @@ class SincosCudaTuner(MeasurementInterface):
 if __name__ == '__main__':
   FAIL_PENALTY = 9999999999
   compiled = False
-  n = 1 * 224 * 224
+  n = 1 * 320 * 320
 
   argparser = opentuner.default_argparser()
   SincosCudaTuner.main(argparser.parse_args())
