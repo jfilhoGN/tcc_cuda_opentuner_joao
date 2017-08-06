064810850850677298354d0e9a134fdd361fce32
João Martins Filho <joaomfilho1995@gmail.com>
2017-08-06 12:17:48 -0300

    add sum 512 inst

--- wscad/sumvector-coalescence/sumvector-opentuner.py
+++ wscad/sumvector-coalescence/sumvector-opentuner.py
@@ -20,14 +20,14 @@ from opentuner import Result
 
 BLOCO_PARAMETROS = [
 	('kernel', 0, 0), 
-	('n', 128, 128),
+	('n', 512, 512),
 	('gpuId', 0, 0)  
 ]
 
 BLOCO_PARAMETROS_CONFIGS = [ 'config' ]
 
 def read_file_configs():
-  file_sumvector = open('/home/projetocuda/Documentos/tcc_cuda_opentuner_joao/wscad/gen-configs/saida_sumvector-128.txt','r')
+  file_sumvector = open('/home/projetocuda/Documentos/tcc_cuda_opentuner_joao/wscad/gen-configs/saida_sumvector-512.txt','r')
   list_configs = []
   for linha in file_sumvector:
     list_configs.append(linha)
@@ -179,6 +179,6 @@ class SumVectorTuner(MeasurementInterface):
 if __name__ == '__main__':
 	FAIL_PENALTY = 9999999999
 	compiled = False
-	n = 128
+	n = 512
 	argparser = opentuner.default_argparser()
 	SumVectorTuner.main(argparser.parse_args())
