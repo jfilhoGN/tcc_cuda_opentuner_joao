b0da340ecb197b5bc09dbac10b0ef83de3727ad7
João Martins Filho <joaomfilho1995@gmail.com>
2017-08-06 18:32:42 -0300

    Correcoes sum 131072 inst

--- wscad/sumvector-coalescence/sumvector-opentuner.py
+++ wscad/sumvector-coalescence/sumvector-opentuner.py
@@ -159,7 +159,7 @@ class SumVectorTuner(MeasurementInterface):
 			strg = "" + current_line
 			if strg.find("Instructions Executed") > -1:
 				idx = strg.index("Instructions Executed")
-				subsrtg = strg[idx:].split("    ")
+				subsrtg = strg[idx:].split("   ")
 				print "substrg: ", subsrtg
 				#substring = subsrtg[3]
 				#substring1 = substring.replace("%",'')
