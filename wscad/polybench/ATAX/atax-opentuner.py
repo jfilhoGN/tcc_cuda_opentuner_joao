a919a45bb45baabfa7e2541091eca8020ca56df8
João Martins Filho <joaomfilho1995@gmail.com>
2017-08-08 23:19:39 -0300

    atax inst 64

--- wscad/polybench/ATAX/atax-opentuner.py
+++ wscad/polybench/ATAX/atax-opentuner.py
@@ -149,7 +149,7 @@ class ataxTuner(MeasurementInterface):
 			strg = "" + current_line
 			if strg.find("Instructions Executed") > -1:
 				idx = strg.index("Instructions Executed")
-				subsrtg = strg[idx:].split("    ")
+				subsrtg = strg[idx:].split("  ")
 				print "substrg: ", subsrtg
 				metric_value = float(subsrtg[3])
 				print "inst_executed: ", metric_value
