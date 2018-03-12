bb10a07d84aea45d824d4b58552b47e11d6122db
João Martins Filho <joaomfilho1995@gmail.com>
2018-03-12 15:20:27 -0300

    modificacoes script.r para analise exploratória

--- pattern_IA/testes/script.r
+++ pattern_IA/testes/script.r
@@ -1,2 +1,4 @@
-dados<-read.csv("/home/jfilhogn/Documentos/Git/tcc_cuda_opentuner_joao/pattern_IA/testes/gemm-smefficiency-64-opentuner.csv",header = T)
+dados<-read.csv("/home/joao/Documentos/tcc_cuda_opentuner_joao/pattern_IA/testes/gemm-smefficiency-512-todas-conf-gtx780.csv",header = T)
 is.data.frame(dados)
+dados
+summary(dados)
