3f1044195941078ad0cd4cc98c83fc52b6a6004e
João Martins Filho <joaomfilho1995@gmail.com>
2018-03-13 21:48:58 -0300

    Adicionado script analise Exploratoria

--- pattern_IA/testes/script.r
+++ pattern_IA/testes/script.r
@@ -1,4 +1,99 @@
-dados<-read.csv("/home/joao/Documentos/tcc_cuda_opentuner_joao/pattern_IA/testes/gemm-smefficiency-512-todas-conf-gtx780.csv",header = T)
+mypath <- "/home/jfilhogn/Documentos/Git/tcc_cuda_opentuner_joao/pattern_IA/testes/gemm-smefficiency-512-todas-conf-gtx780.csv"
+
+dados <- read.csv(mypath, header = T, sep = ",")
 is.data.frame(dados)
-dados
+head(dados)
+str(dados)
 summary(dados)
+
+# adiciona interacao entre g e b
+dados <- transform(dados, 
+                   g=interaction(gx,gy,gz),
+                   b=interaction(bx,by,bz))
+
+# limpa os níveis não utilizados
+dados <- droplevels(dados)
+
+
+## graficos de densidade da variavel smefficiency
+with(dados, plot(density(smefficiency), ylim = c(0, 0.1)))
+with(dados, hist(smefficiency, add = TRUE, freq = FALSE))
+
+## escala log
+with(dados, plot(density(log(smefficiency)), ylim = c(0, 2)))
+with(dados, hist(log(smefficiency), add = TRUE, freq = FALSE))
+
+
+options(max.print = 100000000)
+
+## tables das variaveis g e b
+with(dados, table(g))
+with(dados, table(b))
+with(dados, plot(table(g)))
+with(dados, plot(table(b)))
+
+
+tabg <- as.data.frame(table(dados$g))
+tabg <- tabg[order(tabg[,1]),]
+
+tabb <- as.data.frame(table(dados$b))
+tabb <- tabb[order(tabb[,2]),]
+
+
+
+## usando funcoes da biblioteca lattice
+library(lattice)
+
+x11()
+xyplot(smefficiency ~ g, data = dados, 
+       as.table = TRUE,
+       scales = list(y = list(cex = 1),
+                     x = list(cex = 0.5, rot = 45))
+       # layout = c(22,20) 
+       )
+
+
+
+dados$cut.efic <- with(dados, cut(smefficiency, 
+                                  breaks = c(0,18, 30, 60, 80, 100)))
+
+with(dados, table(cut.efic))
+
+dados.80.100 <- subset(dados, cut.efic == "(80,100]")
+dados.60.80 <- subset(dados, cut.efic == "(60,80]")
+dados.30.60 <- subset(dados, cut.efic == "(30,60]")
+dados.18.30 <- subset(dados, cut.efic == "(18,30]")
+dados.0.18 <- subset(dados, cut.efic == "(0,18]")
+
+str(dados.60.80)
+
+with(dados.80.100, plot(density(smefficiency), 
+                        # ylim = c(0, 0.1)
+                        ))
+with(dados.80.100, hist(smefficiency, add = TRUE, freq = FALSE))
+
+## escala log
+with(dados.80.100, plot(density(log(smefficiency)), 
+                        # ylim = c(0, 2)
+                        ))
+with(dados.80.100, hist(log(smefficiency), add = TRUE, freq = FALSE))
+
+xyplot(smefficiency ~ b|g, data = dados.80.100, 
+       as.table = TRUE, 
+       scales = list(y = list(cex = 1),
+                     x = list(cex = 0.5, rot = 90))
+       # layout = c(22,20) 
+)
+
+## -----------------------------------------------------------------------------
+
+
+
+
+## Pensando ...
+teste <- as.data.frame(with(dados, table(g)))
+temp <- subset(teste, Freq == 0 )
+
+
+subset(dados, g == "2048.256.1024")
+
